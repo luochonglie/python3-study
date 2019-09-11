fc6cb10ffb9905bf930bbfca5878a4df89c5034c
luochonglie <luochonglie@163.com>
2019-09-11 23:29:55 +0800

    counting by line.

--- read/words_counter.py
+++ read/words_counter.py
@@ -1,6 +1,33 @@
 import re
 
-with open('./data/in.txt') as file:
-    text = file.read()
+word_cnt = {}
 
-print(text)
+
+def parser(txt):
+    # 使用正则表达式去除标点符号和换行符
+    txt = re.sub(r'[^\w ]', ' ', txt)
+
+    # 转为小写
+    txt = txt.lower()
+
+    # 生成所有单词的列表
+    word_list = txt.split(' ')
+
+    # 去除空白单词
+    word_list = filter(None, word_list)
+
+    # 生成单词和词频的字典
+    global word_cnt
+    for word in word_list:
+        if word not in word_cnt:
+            word_cnt[word] = 0
+        word_cnt[word] += 1
+
+
+with open('./data/in.txt') as file:
+    for line in file:
+        parser(line)
+
+
+word_cnt = sorted(word_cnt.items(), key=lambda kv: kv[1], reverse=True)
+print(word_cnt)
