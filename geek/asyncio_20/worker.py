8f0c8892bc208f7dd10897e5ebcddea40ea2efd5
luochonglie <luochonglie@163.com>
2019-09-25 20:53:38 +0800

    call function by args.

--- geek/asyncio_20/worker.py
+++ geek/asyncio_20/worker.py
@@ -1,4 +1,5 @@
 import asyncio
+import sys
 
 
 async def worker_1():
@@ -9,7 +10,7 @@ async def worker_1():
 
 async def worker_2():
     print("worker_2 start")
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     print("worker_2 end")
 
 
@@ -19,13 +20,43 @@ async def worker(name, sleep_time):
     print(f'{name} end')
 
 
-async def main():
-    tasks = [asyncio.create_task(worker(f'worker_{i}', 1)) for i in range(1000)]
+async def main_1():
+    print("main_1 await worker sync")
+    print("before await")
+    await worker_1()
+    print("awaited worker_1")
+    await worker_2()
+    print("awaited worker_2")
+    print("finished")
+
+
+async def main_2():
+    print("main_2 await worker async")
+    task1 = asyncio.create_task(worker_1())
+    task2 = asyncio.create_task(worker_2())
+    print("before await")
+    await task1
+    print("awaited worker_1")
+    await task2
+    print("awaited worker_2")
+    print("finished")
+
+
+async def main_3(num=10, sleep_time=1):
+    print(f"main_3 await [{num}] workers async")
+    tasks = [asyncio.create_task(worker(f'worker_{i}', sleep_time)) for i in range(num)]
     print("before await")
     for i, task in enumerate(tasks):
         await task
         print(f"awaited worker_{i}")
 
 
+async def main():
+    main_3(10)
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    if len(sys.argv) > 1:
+        asyncio.run(eval(sys.argv[1])())
+    else:
+        asyncio.run(main())
