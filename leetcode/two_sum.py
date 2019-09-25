09ab956290ded8e446c1af62e18cb2d9e7ac51a5
luochonglie <luochonglie@163.com>
2019-09-25 21:31:51 +0800

    call function by args.

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -2,18 +2,41 @@ from typing import List
 
 
 class Solution:
-	def twoSum(self, nums: List[int], target: int) -> List[int]:
-		for i1, v1 in enumerate(nums):
-			for i2, v2 in enumerate(nums[i1 + 1:]):
-				if v1 + v2 == target:
-					return [i1, i1 + i2 + 1]
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        for i1, v1 in enumerate(nums):
+            for i2, v2 in enumerate(nums[i1 + 1:]):
+                if v1 + v2 == target:
+                    return [i1, i1 + i2 + 1]
 
-		return []
+        return []
+
+    def two_sum_map_loop_twice(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for i, v in enumerate(nums):
+            num_map[v] = i
+
+        for i, v in enumerate(nums):
+            j = num_map.get(target - v)
+            if j is not None and i != j:
+                return [i, j]
+
+        return []
+
+    def two_sum_map_loop_twice(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for i, v in enumerate(nums):
+            num_map[v] = i
+
+        for i, v in enumerate(nums):
+            j = num_map.get(target - v)
+            if j is not None and i != j:
+                return [i, j]
+
+        return []
 
 
 nums = [1, 3, 7, 8, 9, 5, 2]
 target = 5
 
 s = Solution()
-print(s.twoSum(nums, target))
-
+print(s.two_sum_map_loop_twice(nums, target))
