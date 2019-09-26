1be43b0017f5fb18436117a6b4442bfbf0952703
luochonglie <luochonglie@163.com>
2019-09-26 15:25:16 +0000

    add futeres

---
+++ geek/20_futures/url_len.py
@@ -0,0 +1,47 @@
+import sys
+import requests
+import concurrent.futures
+import threading
+sys.path[0] = '../../'
+
+
+
+from geek.decorator import time_consuming
+
+
+def download_one(url):
+	resp = requests.get(url)
+	print(f'read {len(resp.content)} bytes from {url}')
+
+
+@time_consuming
+def download_all(sites):
+	for site in sites:
+		download_one(site)
+
+
+@time_consuming
+def download_all_thread(sites):
+	with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+		executor.map(download_one, sites)
+
+
+@time_consuming
+def download_all_process(sites):
+	with concurrent.futures.ProcessPoolExecutor(2) as executor:
+		executor.map(download_one, sites)
+
+
+def main():
+	sites = list(
+		f'http://quanxue.cn/CT_NanHuaiJin/LunYu/LunYu{str(x).zfill(2)}.html'
+		for x in range(2, 30))
+
+	download_all(sites)
+	download_all_thread(sites)
+	#download_all_process(sites)
+
+
+if __name__ == '__main__':
+	main()
+
