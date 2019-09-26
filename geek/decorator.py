0661281ad34da82313ba4d0972b8e0648d118542
luochonglie <luochonglie@163.com>
2019-09-26 15:23:52 +0000

    time_consuming ms -> s

--- geek/decorator.py
+++ geek/decorator.py
@@ -3,22 +3,23 @@ import functools
 
 
 def time_consuming(func):
-    @functools.wraps(func)
-    def wraper(*args, **kwargs):
-        begin = time.perf_counter()
-        res = func(*args, **kwargs)
-        end = time.perf_counter()
-        duration = (end - begin) * 1000
-        print(f'{func.__name__} took {duration} ms.')
-        return res
+	@functools.wraps(func)
+	def wraper(*args, **kwargs):
+		begin = time.perf_counter()
+		res = func(*args, **kwargs)
+		end = time.perf_counter()
+		duration = end - begin
+		print(f'{func.__name__} took {duration} s.')
+		return res
 
-    return wraper
+	return wraper
 
 
 @time_consuming
 def main():
-    print("Hello")
+	print("Hello")
 
 
 if __name__ == '__main__':
-    main()
+	main()
+
