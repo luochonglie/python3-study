f0ba11c3adafdc214e403355211add5d1c9c0669
luochonglie <luochonglie@163.com>
2019-10-01 06:35:45 +0800

    fixed [-9999]

--- leetcode/three_sum.py
+++ leetcode/three_sum.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-# TODO fix logic issue
+# TODO fix [0,0,0,0]
 class Solution:
     def three_sum(self, nums: List[int]) -> List[List[int]]:
         if len(nums) < 3:
@@ -14,14 +14,18 @@ class Solution:
             t_sum = nums[i] + nums[j] + nums[k]
             if t_sum == 0:
                 result.append([nums[i], nums[j], nums[k]])
-                i, j = i + 1, i + 2
+                k -= 1
+                i, j = 0, 1
             elif t_sum < 0:
                 if j == k - 1:
                     i, j = i + 1, i + 2
                 else:
                     j += 1
             else:
-                k -= 1
-                j = i + 1
+                if i == 0 and j == 1 or (nums[i] > 0 and nums[j] > 0):
+                    k -= 1
+                    i, j = 0, 1
+                else:
+                    i, j = i + 1, i + 2
 
         return result
