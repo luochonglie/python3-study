feff35829e9695dca4c6f41014533fba08a3b7d5
luochonglie <luochonglie@163.com>
2019-10-01 00:37:44 +0800

    add test case for three sum.

--- leetcode/three_sum.py
+++ leetcode/three_sum.py
@@ -3,46 +3,29 @@ from typing import List
 
 # TODO performance issue
 class Solution:
-	def three_sum(self, nums: List[int]) -> List[List[int]]:
-		if len(nums) < 3:
-			return 
-		two_sum_map = {}
-		num_count = {}
-		# caching two sum
-		for i in range(0, len(nums) - 1):
-			if nums[i] not in num_count:
-				num_count[nums[i]] = nums.count(nums[i])
-
-			for j in range(i + 1, len(nums)):
-				two_sum = nums[i] + nums[j]
-				if two_sum in two_sum_map:
-					two_sum_tuple = sorted((nums[i], nums[j]))
-					if two_sum_tuple not in two_sum_map.get(two_sum):
-						two_sum_map.get(two_sum).append(two_sum_tuple)
-				else:
-					two_sum_map[two_sum] = [sorted((nums[i], nums[j]))]
-
-		if nums[-1] not in num_count:
-			num_count[nums[-1]] = nums.count(nums[-1])
-
-		result = set()
-		for num in nums:
-			gap = 0 - num
-			if gap in two_sum_map:
-				for two_sum_list in two_sum_map.get(gap):
-					if (two_sum_list.count(num) + 1) <= num_count[num]:
-						result.add(tuple(sorted(two_sum_list + [num])))
-
-		return [list(x) for x in result]
-
-
-def main():
-	nums = [[-1, 0, 1, 2, -1, -4], [0, 0, 0], []]
-	s = Solution()
-	for v in nums:
-		print(s.three_sum(v))
-
-
-if __name__ == '__main__':
-	main()
-
+    def three_sum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res, k = [], 0
+        for k in range(len(nums) - 2):
+            if nums[k] > 0: break  # 1. because of j > i > k.
+            if k > 0 and nums[k] == nums[k - 1]: continue  # 2. skip the same `nums[k]`.
+            i, j = k + 1, len(nums) - 1
+            while i < j:  # 3. double pointer
+                s = nums[k] + nums[i] + nums[j]
+                if s < 0:
+                    i += 1
+                    while i < j and nums[i] == nums[i - 1]:
+                        i += 1
+                elif s > 0:
+                    j -= 1
+                    while i < j and nums[j] == nums[j + 1]:
+                        j -= 1
+                else:
+                    res.append([nums[k], nums[i], nums[j]])
+                    i += 1
+                    j -= 1
+                    while i < j and nums[i] == nums[i - 1]:
+                        i += 1
+                    while i < j and nums[j] == nums[j + 1]:
+                        j -= 1
+        return res
